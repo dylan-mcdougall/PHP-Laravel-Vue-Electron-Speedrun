f8d23ace96e4883244fbbfd07241e5045d4c5c13
Dylan McDougall <dylan.7mcdougall@gmail.com>
2024-04-30 13:15:33 -0700

    solved vite build and vue component rendering

--- laravel/resources/js/app.js
+++ laravel/resources/js/app.js
@@ -1,1 +1,8 @@
 import './bootstrap';
+import { createApp, defineComponent } from 'vue';
+import Main from './components.vue/main.vue'
+
+const app = createApp({})
+
+app.component('mainapp', Main);
+app.mount('#app');
---
+++ laravel/resources/js/components.vue/main.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { ref } from 'vue';
+
+const counter = ref(0);
+</script>
+
+<template>
+    <div>
+        <h1>
+            This is the first component.
+        </h1>
+        <button
+        type="button"
+        @click="counter++"
+        class="p-2 text-white bg-gray-500 rounded">
+        Counter is: {{ counter }}
+        </button>
+    </div>
+</template>
